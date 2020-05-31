95a3f73ee800a6a5652b18f4a7a1cadba879bfd2
DivadNojnarg <dgranjon@ymail.com>
2020-06-01 00:10:18 +0200

    add tabler action button

--- R/tabler.R
+++ R/tabler.R
@@ -473,3 +473,61 @@ tabler_ribbon <- function(..., position = NULL, color = NULL, bookmark = FALSE) 
   )
   div(class = ribbon_cl, ...)
 }
+
+
+
+
+#' Custom Tabler action button
+#'
+#' @inheritParams shiny::actionButton
+#' @param status Button color.
+#' @return A Tabler action button tag.
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(shiny)
+#'  ui <- tabler_page(
+#'   tabler_body(
+#'     tabler_button(
+#'       "btn",
+#'       HTML(paste("Value", textOutput("val"), sep = ":")),
+#'       icon = icon("thumbs-up"),
+#'       width = "25%"
+#'     )
+#'   )
+#'  )
+#'
+#'  server <- function(input, output) {
+#'    output$val <- renderText(input$btn)
+#'  }
+#'
+#'  shinyApp(ui, server)
+#' }
+tabler_button <- function(inputId, label, status = NULL, icon = NULL, width = NULL, ...) {
+
+  btn_cl <- paste0(
+    "btn action-button",
+    if (is.null(status)) {
+      " btn-primary"
+    } else {
+      paste0(" btn-", status)
+    }
+  )
+
+  value <- restoreInput(id = inputId, default = NULL)
+
+  # custom right margin
+  if (!is.null(icon)) icon$attribs$class <- paste0(
+    icon$attribs$class, " mr-1"
+  )
+
+  tags$button(
+    id = inputId,
+    style = if (!is.null(width)) paste0("width: ", validateCssUnit(width), ";"),
+    type = "button",
+    class = btn_cl,
+    `data-val` = value,
+    list(icon, label), ...
+  )
+}
