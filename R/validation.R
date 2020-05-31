73accb40586f2c04e3efecf5df4e69be1040c384
DivadNojnarg <dgranjon@ymail.com>
2020-05-31 23:22:27 +0200

    validate status and padding

--- R/validation.R
+++ R/validation.R
@@ -1,6 +1,6 @@
 #' Validation function
 #'
-#' Validate a Tabler element width.
+#' Validate a Tabler element width like \link{tabler_card}.
 #'
 #' @param width Value to validate.
 #'
@@ -18,5 +18,71 @@ validate_width <- function(width) {
     }
   } else {
     stop("width must be numeric")
+  }
+}
+
+
+
+valid_statuses <- c(
+  "primary",
+  "secondary",
+  "success",
+  "info",
+  "warning",
+  "danger",
+  "light",
+  "dark"
+)
+
+#' Validation function
+#'
+#' Validate the status of a Tabler element like \link{tabler_card}.
+#'
+#' @param status Color to validate.
+#'
+#' @return TRUE if the test pass.
+#' @export
+#'
+#' @examples
+#' validate_status("danger")
+#' validate_status("maroon")
+validate_status <- function(status) {
+
+  if (is.null(status)) {
+    return(TRUE)
+  } else {
+    if (status %in% valid_statuses) {
+      return(TRUE)
+    }
+  }
+
+  stop("Invalid status: ", status, ". Valid statuses are: ",
+       paste(valid_statuses, collapse = ", "), ".")
+}
+
+
+
+
+valid_paddings <- c("sm", "md", "lg")
+
+#' Validation function
+#'
+#' Validate a Tabler card padding. See \link{tabler_card}
+#' for more informations.
+#'
+#' @param padding Padding value to validate.
+#'
+#' @return An error is raised if the input padding is not valid.
+#' @export
+#'
+#' @examples
+#' validate_padding("xs")
+#' validate_padding("sm")
+validate_padding <- function(padding) {
+  if (!is.null(padding)) {
+    if (!(padding %in% valid_paddings)) {
+      stop("Invalid status: ", padding, ". Valid choices are: ",
+           paste(valid_paddings, collapse = ", "), ".")
+    }
   }
 }
