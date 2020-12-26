2ded82842ce3aa62d401fd3877e249b8fc704d08
DivadNojnarg <dgranjon@ymail.com>
2020-12-26 01:18:46 +0100

    also update aria-valuenow for progress ...

--- inst/tabler/tabler_progress_handler.js
+++ inst/tabler/tabler_progress_handler.js
@@ -1,5 +1,7 @@
 $(function() {
   Shiny.addCustomMessageHandler('update-progress', function(message) {
-    $('#' + message.id).css('width', message.value +'%');
+    $('#' + message.id)
+     .css('width', message.value +'%')
+     .attr('aria-valuenow', message.value);
   });
 });
