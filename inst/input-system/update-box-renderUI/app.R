df1f5512978619b6649ff4d5102443d9878c84b7
DivadNojnarg <dgranjon@ymail.com>
2021-08-25 17:50:01 +0200

    add box examples

---
+++ inst/input-system/update-box-renderUI/app.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(shinyWidgets)
+library(OSUICode)
+
+ui <- fluidPage(
+  # import shinydashboard deps without the need of the
+  # dashboard template
+  useShinydashboard(),
+
+  tags$style("body { background-color: ghostwhite};"),
+
+  br(),
+  uiOutput("custom_box"),
+  selectInput(
+    "background",
+    "Background",
+    choices = shinydashboard:::validColors
+  )
+)
+
+server <- function(input, output, session) {
+
+  dummy_task <- reactive(Sys.sleep(5))
+
+  output$custom_box <- renderUI({
+    dummy_task()
+    box2(
+      title = "Box",
+      "Box body",
+      background = input$background
+    )
+  })
+}
+
+shinyApp(ui, server)
