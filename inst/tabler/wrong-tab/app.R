b57388de894b616e9fa8c483e6795c841a96de07
DivadNojnarg <dgranjon@ymail.com>
2021-09-07 11:48:13 +0200

    add extra demo for tab validation

--- inst/tabler/wrong-tab/app.R
+++ inst/tabler/wrong-tab/app.R
@@ -1,29 +1,22 @@
 library(shiny)
 library(OSUICode)
-library(thematic)
 
-thematic_shiny()
-
-# example with input binding: inputId is required for tabler_navbar_menu!!!
 ui <- tabler_page(
   tabler_navbar(
     brand_url = "https://preview-dev.tabler.io",
     brand_image = "https://preview-dev.tabler.io/static/logo.svg",
     nav_menu = tabler_navbar_menu(
-      id = "current_tab",
+      inputId = "mymenu",
       tabler_navbar_menu_item(
         text = "Tab 1",
-        icon = NULL,
         tabName = "tab1",
         selected = TRUE
       ),
       tabler_navbar_menu_item(
         text = "Tab 2",
-        icon = NULL,
-        tabName = "tab2"
+        tabName = "hello%%&1"
       )
-    ),
-    tabler_button("update", "Change tab", icon = icon("exchange-alt"))
+    )
   ),
   tabler_body(
     tabler_tab_items(
@@ -39,32 +32,19 @@ ui <- tabler_page(
         plotOutput("distPlot")
       ),
       tabler_tab_item(
-        tabName = "tab2",
+        tabName = "hello%%&1",
         p("Second Tab")
       )
     ),
     footer = tabler_footer(
       left = "Rstats, 2020",
-      right = a(href = "https://www.google.com")
+      right = a(href = "https://www.google.com", "More")
     )
   )
 )
-server <- function(input, output, session) {
+server <- function(input, output) {
   output$distPlot <- renderPlot({
     hist(rnorm(input$obs))
-  })
-
-  observeEvent(input$current_tab, {
-    showNotification(
-      paste("Hello", input$current_tab),
-      type = "message",
-      duration = 1
-    )
-  })
-
-  observeEvent(input$update, {
-    newTab <- if (input$current_tab == "tab1") "tab2" else "tab1"
-    update_tabler_tab_item("current_tab", newTab)
   })
 }
 shinyApp(ui, server)
