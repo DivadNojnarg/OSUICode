0ba1c35b85342c6933cc56489bae41186cc01601
DivadNojnarg <dgranjon@ymail.com>
2021-09-08 23:35:50 +0200

    update pwa example

--- inst/shinyMobile/pwa/app.R
+++ inst/shinyMobile/pwa/app.R
@@ -2,39 +2,13 @@ library(shiny)
 library(OSUICode)
 
 ui <- f7_page(
-  f7_gauge(
-    "mygauge",
-    value = 0.1,
-    options = list(
-      type = "semicircle",
-      borderColor = "#2196f3",
-      borderWidth = 10,
-      valueFontSize = 41,
-      valueTextColor = "#2196f3",
-      labelText = "amount of something"
-    )
-  ),
-  navbar = f7_navbar("Title"),
+  navbar = f7_navbar("PWA App"),
   toolbar = f7_toolbar(),
-  title = "shinyMobile",
-  options = list(
-    theme = "ios",
-    version = "1.0.0",
-    touch = list(
-      tapHold = TRUE
-    ),
-    color = "#42f5a1",
-    filled = TRUE,
-    dark = TRUE
-  )
+  title = "shinyMobile"
 )
 
 server <- function(input, output, session) {
   session$allowReconnect("force")
-  observe({
-    Sys.sleep(2)
-    update_f7_instance("mygauge", options = list(value = 0.75, valueText = "75 %", labelText = "New label!"))
-  })
 }
 
 shinyApp(ui, server)
